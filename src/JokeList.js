f1257f043f313f25193fb5e5d58fa9f640010d2b
elena-vost <angara1298@gmail.com>
2021-02-25 17:41:21 +0100

    new joks

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import axios from "axios";
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10,
+  };
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
   async componentDidMount() {
-    let res = await axios.get("https://icanhazdadjoke.com/", {
-      headers: { Accept: "application/json" },
-    });
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" },
+      });
+      jokes.push(res.data.joke);
+    }
+    this.setState({ jokes: jokes });
   }
   render() {
     return (
-      <div>
+      <div className="JokeList">
         <h1>Joke List</h1>
+        <div className="JokeList-jokes">
+          {this.state.jokes.map((j) => (
+            <div>{j}</div>
+          ))}
+        </div>
       </div>
     );
   }
