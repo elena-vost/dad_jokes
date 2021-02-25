dcaa7cedf6c93c73ba8c1572b532fa1994158a98
elena-vost <angara1298@gmail.com>
2021-02-25 17:26:40 +0100

    joke list

---
+++ src/JokeList.js
@@ -0,0 +1,11 @@
+import React, { Components } from "react";
+class JokeList extends Components {
+  render() {
+    return (
+      <div>
+        <h1>Joke List</h1>
+      </div>
+    );
+  }
+}
+export default JokeList;
